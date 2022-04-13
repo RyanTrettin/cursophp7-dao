bf9fe17577244dd03b2110e55858eac21ef889dc
unknown <ryantrettin1@gmail.com>
2022-04-13 14:29:36 -0300

    Classe Usuario criada

--- index.php
+++ index.php
@@ -2,12 +2,9 @@
 
 require_once("config.php");
 
-$sql = new Sql();
+$root = new Usuario();
 
-$usuarios = $sql->select("INSERT INTO tb_usuarios(deslogin, dessenha) VALUES ('Paulo Augusto', '12345678') ");
+$root->loadById(2);
 
-$usuarios = $sql->select("SELECT * FROM tb_usuarios) ");
-
-echo json_encode($usuarios);
-
+echo $root;
 ?>
